478fabd7fb43e55070b51f985c371055a7c69ba6
Scott Kingsley Clark <lol@scottkclark.com>
2016-01-21 12:56:31 -0600

    Initial Pods integration work

--- facetwp-pods.php
+++ facetwp-pods.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: CMB2 Integration for FacetWP
- * Plugin URI: https://github.com/WebDevStudios/facetwp-cmb2
- * Description: Allow FacetWP to properly utilize data created and stored by CMB2.
+ * Plugin Name: Pods Integration for FacetWP
+ * Plugin URI: https://github.com/pods-framework/facetwp-pods
+ * Description: Allow FacetWP to properly utilize data created and stored by the Pods Framework.
  * Version: 1.0.0
- * Author: Matt Gibbs, Jeremy Pry (WebDevStudios)
+ * Author: Matt Gibbs, Scott Kingsley Clark, Jeremy Pry (WebDevStudios)
  * License: GPL2
  */
 
@@ -13,23 +13,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( "You can't do anything by accessing this file directly." );
 }
 
-class FacetWP_Integration_CMB2 {
+class FacetWP_Integration_Pods {
 
 	/**
-	 * @var FacetWP_Integration_CMB2
+	 * @var FacetWP_Integration_Pods
 	 */
 	protected static $instance = null;
 
-	protected function __construct() {}
+	private function __construct() {
+		
+		add_action( 'plugins_loaded', array( $this, 'setup_hooks' ) );
+		
+	}
 
 	/**
-	 * @return FacetWP_Integration_CMB2
+	 * @return FacetWP_Integration_Pods
 	 */
 	public static function instance() {
+
 		if ( is_null( self::$instance ) ) {
 			self::$instance = new self();
 		}
+
 		return self::$instance;
+
 	}
 
 	/**
@@ -39,27 +46,28 @@ class FacetWP_Integration_CMB2 {
 	 */
 	public function setup_hooks() {
 
-		// If we don't have CMB2, then there's nothing to do.
-		if ( ! defined( 'CMB2_LOADED' ) ) {
+		// If we don't have Pods, then there's nothing to do.
+		if ( ! defined( 'PODS_VERSION' ) ) {
 			return;
 		}
 
-		// Add CMB2 fields to the Data Sources dropdown
+		// Add Pods fields to the Data Sources dropdown
 		add_filter( 'facetwp_facet_sources', array( $this, 'facet_sources' ) );
 
-		// CMB2 field handler
+		// Pods field handler
 		add_filter( 'facetwp_indexer_post_facet', array( $this, 'indexer_post_facet' ), 10, 2 );
 
 		// Text fields that should be indexed
-		add_filter( 'facetwp_cmb2_skip_index_text', array( $this, 'text_field_exceptions' ), 10, 2 );
+		add_filter( 'facetwp_pods_skip_index_text', array( $this, 'text_field_exceptions' ), 10, 2 );
 
 		// Special handling for time/date fields
-		add_filter( 'facetwp_cmb2_default_index', array( $this, 'time_date_indexing' ), 10, 3 );
+		add_filter( 'facetwp_pods_default_index', array( $this, 'time_date_indexing' ), 10, 3 );
+
 	}
 
 
 	/**
-	 * Add CMB2 fields to the Data Sources dropdown.
+	 * Add Pods fields to the Data Sources dropdown.
 	 *
 	 * @since 1.0.0
 	 *
@@ -67,29 +75,28 @@ class FacetWP_Integration_CMB2 {
 	 * @return array The updated set of data sources.
 	 */
 	public function facet_sources( $sources ) {
-		if ( ! defined( 'CMB2_LOADED' ) ) {
-			return $sources;
-		}
 
-		$sources['cmb2'] = array(
-			'label'   => 'CMB2',
+		$sources['pods'] = array(
+			'label'   => 'Pods',
 			'choices' => array(),
 		);
 
-		// Get every CMB2 registered field as an array
-		$fields = $this->get_cmb_fields();
+		// Get every Pods registered field as an array
+		$fields = $this->get_pods_fields();
+		
 		foreach ( $fields as $field ) {
-			// The Field ID string is used later to determine the metabox and field ID
-			$field_id_string = "cmb2/{$field['metabox_id']}/{$field['id']}";
+			// The Field ID string is used later to determine the Pod name and Field name
+			$field_id_string = "pods/{$field['pod_name']}/{$field['name']}";
 
-			$sources['cmb2']['choices'][ $field_id_string ] = $field['label'];
+			$sources['pods']['choices'][ $field_id_string ] = $field['label'];
 		}
 
 		return $sources;
+
 	}
 
 	/**
-	 * Index CMB2 field data
+	 * Index Pods field data
 	 *
 	 * @param bool $return
 	 * @param array $params
@@ -97,6 +104,7 @@ class FacetWP_Integration_CMB2 {
 	 * @return bool
 	 */
 	public function indexer_post_facet( $return, $params ) {
+
 		$defaults = $params['defaults'];
 		$facet    = $params['facet'];
 
@@ -107,7 +115,7 @@ class FacetWP_Integration_CMB2 {
 		 *
 		 * @param bool $debugging Whether to enable debugging. Defaults to false./
 		 */
-		if ( apply_filters( 'facetwp_cmb2_debugging', false ) ) {
+		if ( apply_filters( 'facetwp_pods_debugging', false ) ) {
 			error_log( "Param info: " . print_r( $params, true ) . PHP_EOL, 3, WP_CONTENT_DIR . '/facet.log' );
 		}
 
@@ -115,37 +123,40 @@ class FacetWP_Integration_CMB2 {
 		$source = explode( '/', $facet['source'] );
 
 		// Maybe return early. Includes class check, just in case
-		if ( 'cmb2' !== $source[0] || count( $source ) < 3 || ! class_exists( 'CMB2_boxes', false ) ) {
+		if ( 'pods' !== $source[0] || count( $source ) < 3 ) {
 			return $return;
 		}
 
 		// Initial var setup
-		$metabox_id = $source[1];
-		$field_id   = $source[2];
+		$pod_name   = $source[1];
+		$field_name = $source[2];
 
-		// Make sure we can retrieve the Metabox
-		$cmb = CMB2_Boxes::get( $metabox_id );
-		if ( ! $cmb ) {
+		// Make sure the pod exists
+		$pods = pods( $pod_name );
+		
+		if ( ! $pods || $pods->is_valid() ) {
 			return $return;
 		}
 
-		// Make sure the field can be retrieved.
-		$field = $cmb->get_field( $field_id );
+		// Make sure the field exists
+		$field = $pods->fields( $field_name );
+		
 		if ( ! $field ) {
 			return $return;
 		}
 
-		$field_type = $field->type();
+		$field_type = $field['type']
 
 		/**
-		 * Filter the CMB2 field types that do not need to be indexed by FacetWP.
+		 * Filter the Pods field types that do not need to be indexed by FacetWP.
 		 *
 		 * @since 1.0.0
 		 *
 		 * @param array $fields Array of field types that do not need to be indexed.
 		 */
-		$skip_index = apply_filters( 'facetwp_cmb2_skip_index', array( 'title', 'group' ) );
-		if ( in_array( $field->type(), $skip_index ) ) {
+		$skip_index = apply_filters( 'facetwp_pods_skip_index_field_types', array( 'html' ) );
+		
+		if ( in_array( $field_type, $skip_index ) ) {
 			return true;
 		}
 
@@ -160,10 +171,12 @@ class FacetWP_Integration_CMB2 {
 		 * @param bool   $skip       Whether to skip indexing text fields. Default: true.
 		 * @param string $field_type The field type.
 		 */
-		$skip_index_text = apply_filters( 'facetwp_cmb2_skip_index_text', true, $field_type );
-		if ( false !== strpos( $field->type(), 'text' ) && $skip_index_text ) {
+		 // @todo Fix this
+		/*$skip_index_text = apply_filters( 'facetwp_pods_skip_index_text', true, $field_type );
+		
+		if ( false !== strpos( $field_type, 'text' ) && $skip_index_text ) {
 			return false;
-		}
+		}*/
 
 		/**
 		 * Filter to skip indexing WYSIWYG fields.
@@ -174,19 +187,22 @@ class FacetWP_Integration_CMB2 {
 		 *
 		 * @param bool $skip Whether to skip indexing WYSIWYG fields.
 		 */
-		$skip_index_wysiwyg = apply_filters( 'facetwp_cmb2_skip_index_wysiwyg', true );
+		 // @todo Fix this
+		/*$skip_index_wysiwyg = apply_filters( 'facetwp_pods_skip_index_wysiwyg', true );
+		
 		if ( 'wysiwyg' == $field_type && $skip_index_wysiwyg ) {
 			return false;
-		}
+		}*/
 
 		// Checkboxes are either on or off. Only index the "on" value.
-		if ( 'checkbox' == $field_type ) {
+		// @todo Fix this
+		/*if ( 'checkbox' == $field_type ) {
 			if ( 'on' == $field->value() ) {
 				$this->index_field( $field, $defaults );
 			} else {
 				return true;
 			}
-		}
+		}*/
 
 		/**
 		 * Filter whether to do the default indexing.
@@ -194,16 +210,18 @@ class FacetWP_Integration_CMB2 {
 		 * @since 1.0.0
 		 *
 		 * @param bool                     $index    Whether to use the default indexing.
-		 * @param CMB2_Field               $field    The CMB2_Field object.
+		 * @param array                    $field    The Pods field array.
 		 * @param array                    $defaults The array of defaults.
-		 * @param FacetWP_Integration_CMB2 $obj      The current class object.
+		 * @param FacetWP_Integration_Pods $obj      The current class object.
 		 */
-		$default_index = apply_filters( 'facetwp_cmb2_default_index', true, $field, $defaults, $this );
+		$default_index = apply_filters( 'facetwp_pods_default_index', true, $field, $defaults, $this );
+
 		if ( $default_index ) {
 			$this->index_field_values( $field, $defaults );
 		}
 
 		return true;
+
 	}
 
 	/**
@@ -211,15 +229,19 @@ class FacetWP_Integration_CMB2 {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param CMB2_Field $field    Field object.
-	 * @param array      $defaults Array of default values.
+	 * @param array  $field    Field array.
+	 * @param array  $defaults Array of default values.
 	 */
 	public function index_field( $field, $defaults ) {
+
+ 		// @todo Fix this
 		$index = array(
-			'facet_value'         => $field->args( 'name' ),
-			'facet_display_value' => $field->args( 'desc' ) ?: $field->args( 'name' ),
+			'facet_value'         => $field['name'],
+			'facet_display_value' => $field['label'],
 		);
+
 		$this->index_row( $index, $defaults );
+
 	}
 
 	/**
@@ -227,23 +249,27 @@ class FacetWP_Integration_CMB2 {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param CMB2_Field $field    Field object.
-	 * @param array      $defaults Array of default values.
+	 * @param array  $field    Field object.
+	 * @param array  $defaults Array of default values.
 	 */
 	public function index_field_values( $field, $defaults ) {
+
 		$index  = array();
-		$values = (array) $field->escaped_value();
+		$values = (array) $field; // @todo Fix this
 
 		foreach ( $values as $value ) {
 			if ( empty( $value ) ) {
 				continue;
 			}
+
 			$index[] = array(
 				'facet_value'         => $value,
-				'facet_display_value' => $field->args( 'name' ),
+				'facet_display_value' => $field['name'],
 			);
 		}
+
 		$this->index_multiple( $index, $defaults );
+
 	}
 
 	/**
@@ -257,7 +283,9 @@ class FacetWP_Integration_CMB2 {
 	 * @param array $defaults Default values to use when indexing.
 	 */
 	public function index_row( $value, $defaults ) {
+
 		FWP()->indexer->index_row( wp_parse_args( $value, $defaults ) );
+
 	}
 
 	/**
@@ -269,16 +297,18 @@ class FacetWP_Integration_CMB2 {
 	 * @param array $defaults Default values to use when indexing.
 	 */
 	public function index_multiple( $values, $defaults ) {
+
 		// Loop through each value and index it
 		foreach ( $values as $value ) {
 			$this->index_row( $value, $defaults );
 		}
+
 	}
 
 	/**
 	 * Filter the text fields that should be skipped.
 	 *
-	 * This also serves as an example for how to use the 'facetwp_cmb2_skip_index_text' filter.
+	 * This also serves as an example for how to use the 'facetwp_pods_skip_index_text' filter.
 	 *
 	 * @since 1.0.0
 	 *
@@ -288,6 +318,10 @@ class FacetWP_Integration_CMB2 {
 	 * @return bool
 	 */
 	public function text_field_exceptions( $index, $field_type ) {
+
+		return $index;
+		
+		// @todo Fix this
 		$exception = array(
 			'text_date',
 			'text_time',
@@ -301,28 +335,31 @@ class FacetWP_Integration_CMB2 {
 		}
 
 		return $index;
+
 	}
 
 	/**
 	 * Handle indexing the various date/time fields.
 	 *
-	 * This method also serves as an example of the 'facetwp_cmb2_default_index' filter, although it should be noted
+	 * This method also serves as an example of the 'facetwp_pods_default_index' filter, although it should be noted
 	 * that since it is part of the class, it does not use the $obj class object, but makes method calls directly.
 	 * For use outside of this class, be sure to use $obj->index_row() or $obj->index_multiple().
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param bool       $filter   Continue with normal indexing.
-	 * @param CMB2_Field $field    The field object.
-	 * @param array      $defaults Array of default data.
+	 * @param bool   $filter   Continue with normal indexing.
+	 * @param array  $field    The field array.
+	 * @param array  $defaults Array of default data.
 	 *
 	 * @return bool Whether to continue with the normal indexing.
 	 */
 	public function time_date_indexing( $filter, $field, $defaults ) {
+
+		// @todo Fix this
 		$date_format = 'Y-m-d';
 		$extended_format = "{$date_format} H:i:s";
 		$index = array(
-			'facet_display_value' => $field->args( 'name' ),
+			'facet_display_value' => $field['name'],
 		);
 
 		// Check for special field types
@@ -355,18 +392,20 @@ class FacetWP_Integration_CMB2 {
 	}
 
 	/**
-	 * Get registered CMB2 fields.
+	 * Get registered Pods fields.
 	 *
 	 * @return array Multidimensional array of field data. Each array item contains 'id', 'label',
 	 *               and 'metabox_id' keys.
 	 */
-	protected function get_cmb_fields() {
+	protected function get_pods_fields() {
+		
 		$return = array();
-		if ( ! class_exists( 'CMB2_Boxes', false ) ) {
-			return $return;
-		}
 
+		return $return;
+		
+		// @todo Fix this
 		$boxes  = CMB2_Boxes::get_all();
+
 		foreach ( $boxes as $cmb ) {
 			// Secret override method to skip indexing a metabox's fields
 			if ( $cmb->prop( 'no_facetwp_index', false ) ) {
@@ -383,7 +422,8 @@ class FacetWP_Integration_CMB2 {
 			 *
 			 * @param bool $skip_false_hookup Whether to skip metaboxes with hookup => false.
 			 */
-			$skip_false_hookup = apply_filters( 'facetwp_cmb2_skip_false_hookup', true );
+			$skip_false_hookup = apply_filters( 'facetwp_pods_skip_false_hookup', true );
+
 			if ( $skip_false_hookup && false === $cmb->prop( 'hookup' ) ) {
 				continue;
 			}
@@ -399,7 +439,8 @@ class FacetWP_Integration_CMB2 {
 				 *
 				 * @param bool $skip_hidden_fields Whether to skip indexing hidden fields.
 				 */
-				$skip_hidden_fields = apply_filters( 'facetwp_cmb2_skip_hidden_fields', true );
+				$skip_hidden_fields = apply_filters( 'facetwp_pods_skip_hidden_fields', true );
+
 				if ( $skip_hidden_fields && 'hidden' == $field['type'] ) {
 					continue;
 				}
@@ -413,8 +454,8 @@ class FacetWP_Integration_CMB2 {
 		}
 
 		return $return;
+
 	}
 }
 
-$instance = FacetWP_Integration_CMB2::instance();
-add_action( 'plugins_loaded', array( $instance, 'setup_hooks' ) );
+FacetWP_Integration_Pods::instance();
